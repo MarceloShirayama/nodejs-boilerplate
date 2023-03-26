e7093c9754d8082e7751f9fe65f5913922f98bdf
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-03-26 18:35:52 -0300

    initial settings

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { a } from "./core/a";
+
+console.log(a, process.env["CUSTOM_ENV"]);
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: "istanbul",
+      exclude: ["*/**/errors/*", "test/**/*"],
+      reporter: ["text", "json", "html"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
